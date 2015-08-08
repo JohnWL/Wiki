19ddd9ca3cea46cb8e26cc505bc2a35829e3db42
John Lander <john@linkodyssey.com>
2015-08-08 05:02:06 +0000

    Add views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   
   def index
     @articles = Article.all
@@ -25,6 +26,10 @@ class ArticlesController < ApplicationController
     
     def article_params
       params.require(:article).permit(:title, :content)
+    end
+    
+    def find_article
+      @article = Article.find(params[:id])
     end
   
 end
