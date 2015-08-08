8db258fd024ea60438631ba976f99db3ad6829d1
John Lander <john@linkodyssey.com>
2015-08-08 06:30:44 +0000

    Add edit and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,22 @@ class ArticlesController < ApplicationController
   def show
   end
   
+  def edit
+  end
+  
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+  
   
   private
     
